f4ae0494d13d6f3d0baf2e7c2b862130cd6ab187
73m1 <87555478+73m1@users.noreply.github.com>
2022-07-11 15:44:27 +1000

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+function login() {
+    player_1name = document.getElementById("player1_name").value;
+    player_2name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name", player_1name);
+    localStorage.setItem("player2_name", player_2name);
+
+    window.location.replace("gamepage.html");
+}
